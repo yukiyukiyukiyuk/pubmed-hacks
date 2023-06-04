ceccdf4a8986d8ff4559bd7d295b571b25960366
Ayumu Akagi <ayunonayu5377@gmail.com>
2023-06-05 00:53:11 +0900

    Update urls.py

--- saas_project/urls.py
+++ saas_project/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),#デフォルトで管理画面のルーティングであるpathが記載されている
     path("",include("paper_abstract_app.urls")),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
